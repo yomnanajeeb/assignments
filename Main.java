5a6d4af09ef6bcaa9cbffcddfccdac1872044c4f
yomnanajeeb <125745440+yomnanajeeb@users.noreply.github.com>
2023-02-25 01:29:03 +0200

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args) {
+            Scanner scanner =new Scanner (System.in);
+            System.out.println("enter the first number");
+            int x = scanner. nextInt();
+            System.out.println("enter the second number");
+            int y = scanner. nextInt();
+            System.out.println(x/y) ;
+            System.out.println(x*y) ;
+            System.out.println(x+y) ;
+            System.out.println(x-y);
+    }
+}
