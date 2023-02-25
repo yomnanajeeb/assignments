eac3a78790fe843277aa809fca6ac702338a218a
yomnanajeeb <125745440+yomnanajeeb@users.noreply.github.com>
2023-02-26 01:18:05 +0200

    Add files via upload

---
+++ task1.java
@@ -0,0 +1,18 @@
+public class task1 {
+    public static void main(String[] args) {
+        byte Age = 18;
+        int Number = 40000000;
+        Long idNumber = 8674376519L;
+        double dblink = 2857493.1096856;
+        float Price = 9067.69F;
+        char letter = 'y';
+        boolean classified = false;
+        System.out.println(Age);
+        System.out.println(Number);
+        System.out.println(idNumber);
+        System.out.println(dblink);
+        System.out.println(Price);
+        System.out.println(letter);
+        System.out.println(classified);
+    }
+}
