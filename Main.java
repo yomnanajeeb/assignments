d2e34703ed2817acd7f626ba4a237c16ac6930f0
yomnanajeeb <125745440+yomnanajeeb@users.noreply.github.com>
2023-02-25 02:03:55 +0200

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+            int x = 5;
+            int y = 10;
+            int z = 15;
+            System.out.println((x+y)*(z)/(x-y));
+
+    }
+}
