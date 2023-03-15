9c6626752427a00ecf19714713a49066de081eb6
yomnanajeeb <125745440+yomnanajeeb@users.noreply.github.com>
2023-03-15 23:09:20 +0200

    Add files via upload

--- question1.java
+++ question1.java
@@ -1,12 +1,16 @@
 import java.util.Scanner;
+
 public class question1 {
-    public static void main(String[] args) {
-        Scanner scanner = new Scanner (System.in);
-        System.out.print("Enter the radius of the circle: ");
-        double radius = scanner.nextDouble();
-        scanner.close();
-        double area = Math.PI * radius * radius;
-        int roundedArea = (int) Math.round(area);
-        System.out.println("The area of the circle is: " + roundedArea);
+    public static void main(String[] args){
+
+        Scanner scanner =new Scanner(System.in);
+        System.out.println("please enter number");
+        int x =scanner.nextInt();
+        if (x%2==0)
+            System.out.println("number is even");
+        else
+            System.out.println("number is odd");
+
+
     }
 }
